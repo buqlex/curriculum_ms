17c0422c4bdcdb5d4fb03ac351fd21f492617434
buqlex <buqlex@gmail.com>
2024-06-05 21:15:47 +0300

    integration email sending using SMTP

--- curriculum_ms/settings.py
+++ curriculum_ms/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'buqlex.feedback@outlook.com'
