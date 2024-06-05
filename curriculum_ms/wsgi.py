fb8ee99add8d75814a1cb04dd19be789cdf0faa9
buqlex <buqlex@gmail.com>
2024-06-05 17:06:14 +0300

    update wsgi.py

--- curriculum_ms/wsgi.py
+++ curriculum_ms/wsgi.py
@@ -8,21 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-# Add the project directory to the sys.path
-project_home = '/home/buqlex/curriculum_ms'
-if project_home not in sys.path:
-    sys.path.append(project_home)
-
-# Point to the application settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'curriculum_ms.settings'
-
-# Activate the virtual environment
-activate_env = os.path.expanduser('/home/buqlex/.virtualenvs/venv/bin/activate_this.py')
-with open(activate_env) as f:
-    exec(f.read(), {'__file__': activate_env})
-
-# Import and set up the WSGI application
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'curriculum_ms.settings')
+
 application = get_wsgi_application()
